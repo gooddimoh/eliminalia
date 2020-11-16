b0a36a6f6fa3252b66006370b7bb8fee4823aeb7
Dmitriy <gooddimoh@gmail.com>
2020-11-16 20:39:01 +0200

    update

--- routes/web.php
+++ routes/web.php
@@ -49,18 +49,13 @@ Route::get('dashboard/map/vector', 'MainController@mapVector')->name('map-vector
 Route::resource('UserResource', 'UsersController');
 
 Route::group(['middleware' => 'web'], function () {
-    // Authentication Routes...
-    Route::get( 'user/login',  ['as' => 'user.login',     'uses' => 'Auth\AuthController@showLoginForm']);
-    Route::post('user/login',  ['as' => 'user.doLogin',   'uses' => 'Auth\AuthController@login'        ]);
-
-    Route::group(['middleware' => 'auth'], function() {
-        // Authenticated user routes...
-        Route::get( '/',     ['as'=>'home', 'uses'=> 'HomeController@index']);
-        Route::get( '/home', ['as'=>'home', 'uses'=> 'HomeController@home']);
-        Route::get( 'user/logout', ['as' => 'user.logout',    'uses' => 'Auth\AuthController@logout'  ]);
-
-        // *** Added /profile/ here to prevent matching with other routes ****
-        Route::get( 'user/profile/{uid?}', ['as' => 'user.profile',   'uses' => 'UserController@profile' ]);
-        Route::get( '/user/add',           ['as' => 'user.add',       'uses' => 'UserController@showAddUser']);
+    Route::get('user/login', ['as' => 'user.login', 'uses' => 'Auth\AuthController@showLoginForm']);
+    Route::post('user/login', ['as' => 'user.doLogin', 'uses' => 'Auth\AuthController@login']);
+    Route::group(['middleware' => 'auth'], function () {
+        Route::get('/', ['as' => 'home', 'uses' => 'HomeController@index']);
+        Route::get('/home', ['as' => 'home', 'uses' => 'HomeController@home']);
+        Route::get('/user/logout', ['as' => 'user.logout', 'uses' => 'Auth\AuthController@logout']);
+        Route::get('/user/profile/{uid?}', ['as' => 'user.profile', 'uses' => 'UserController@profile']);
+        Route::get('/user/add', ['as' => 'user.add', 'uses' => 'UserController@showAddUser']);
     });
 });
