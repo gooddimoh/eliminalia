3e3203a54915b260b8665b2f46dd77ce6e20d297
Dmitriy <gooddimoh@gmail.com>
2021-01-08 20:34:40 +0200

    update at 20:33

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -1,14 +1,19 @@
 <?php
+
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
 
-class AuthController extends Controller {
+class AuthController extends Controller
+{
 
     public function getlogin()
     {
+
+//        $user->role = "superadmin";
+
         return view('auth.login');
     }
 
